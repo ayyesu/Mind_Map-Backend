65689dd34a6127d44a31d42506b703a1b509cf84
ayyesu <iamdanielkissiedu@gmail.com>
2023-08-18 00:55:51 +0000

    added: Users schema

---
+++ model/Users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        min: 3,
+        max: 20,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 50,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
