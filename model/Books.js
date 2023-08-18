673d8e26f1cd692e300c177534dde04033570bb4
ayyesu <iamdanielkissiedu@gmail.com>
2023-08-18 00:59:58 +0000

    added: Books schema

---
+++ model/Books.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: false,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    coverImage: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Book', bookSchema);
