161e4c488df352b47bf3a560d097df4dd1cdc6fa
ayyesu <iamdanielkissiedu@gmail.com>
2023-08-18 01:06:12 +0000

    added: Orders schema

---
+++ model/Orders.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    books: [
+        {
+            bookId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Book',
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    paymentId: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed', 'cancelled'],
+        default: 'pending',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Order', orderSchema);
