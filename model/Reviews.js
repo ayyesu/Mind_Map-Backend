32c0b2413db89b94a328c76171a6231e62d7699f
ayyesu <iamdanielkissiedu@gmail.com>
2023-08-18 01:02:22 +0000

    added: Review schema

---
+++ model/Reviews.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    bookId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+    comment: {
+        type: String,
+        required: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
