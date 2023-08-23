b11c1787086596127f438c4c15c0ad690b4b3202
ayyesu <iamdanielkissiedu@gmail.com>
2023-08-23 15:41:39 +0000

    added: endpoint to find a single book

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -25,3 +25,15 @@ export const getBooksByCategory = async (req, res) => {
         res.status(404).json({message: error.message});
     }
 };
+
+// Get single book
+export const getSingleBook = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const book = await Books.findById(id);
+        if (!book) return res.status(404).json({message: 'No book found'});
+        res.status(200).json(book);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+};
