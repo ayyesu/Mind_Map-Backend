aa427c8c4467f126bbd8eb48428aa059b014371f
ayyesu <iamdanielkissiedu@gmail.com>
2023-08-23 16:27:47 +0000

    modified: book router setup

--- route/book.js
+++ route/book.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const {
-    getBooks,
-    getBook,
-    createBook,
+    getAllBooks,
+    getBooksByCategory,
+    getSingleBook,
+    searchBooks,
+    addNewBook,
     updateBook,
     deleteBook,
 } = require('../controller/book.controller');
 
 const router = express.Router();
 
-router.get('/', getBooks);
-router.get('/:id', getBook);
-router.post('/', createBook);
+router.get('/', getAllBooks);
+router.get('/:id', getSingleBook);
+router.get('/:category', getBooksByCategory);
+router.get('/search', searchBooks);
+router.post('/', addNewBook);
 router.patch('/:id', updateBook);
 router.delete('/:id', deleteBook);
 
