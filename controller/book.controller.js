61948f631fb686163524b2fa8d184835262d73cb
ayyesu <iamdanielkissiedu@gmail.com>
2023-08-23 15:38:41 +0000

    controller to get all books, and based on category

---
+++ controller/book.controller.js
@@ -0,0 +1,27 @@
+const Books = require('../model/Books');
+
+// Get all books
+export const getAllBooks = async (req, res) => {
+    try {
+        const books = await Books.find();
+        if (!books) return res.status(404).json({message: 'No books found'});
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+};
+
+// Get all books based on category
+export const getBooksByCategory = async (req, res) => {
+    const category = req.params.category;
+    try {
+        const books = await Books.find({category});
+        if (!books)
+            return res
+                .status(404)
+                .json({message: 'No books found in this category'});
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+};
