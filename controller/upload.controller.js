6660f86da6e366713917fa43aae6465a5a1b2edd
ayyesu <iamdanielkissiedu@gmail.com>
2024-02-13 17:26:01 +0000

    Delete files directly from firebase

--- controller/upload.controller.js
+++ controller/upload.controller.js
@@ -1,63 +1,77 @@
-const { storage } = require("../config/firebase.config");
-const { ref, getDownloadURL, uploadBytes } = require("firebase/storage");
-const { giveCurrentDateTime } = require("../util/dateTime");
-const Books = require("../model/Books");
+const {storage} = require('../config/firebase.config');
+const {ref, getDownloadURL, uploadBytes} = require('firebase/storage');
+const {giveCurrentDateTime} = require('../util/dateTime');
+const Books = require('../model/Books');
 
 exports.addFile = async (req, res) => {
-  try {
-    const dateTime = giveCurrentDateTime();
+    try {
+        const dateTime = giveCurrentDateTime();
 
-    const storageRef = ref(
-      storage,
-      `files/${req.file.originalname + "       " + dateTime}`
-    );
+        const storageRef = ref(
+            storage,
+            `files/${req.file.originalname + '       ' + dateTime}`,
+        );
 
-    const metadata = {
-      contentType: req.file.mimetype,
-    };
+        const metadata = {
+            contentType: req.file.mimetype,
+        };
 
-    // Upload the file in the bucket storage using uploadBytes
-    const snapshot = await uploadBytes(storageRef, req.file.buffer, metadata);
+        // Upload the file in the bucket storage using uploadBytes
+        const snapshot = await uploadBytes(
+            storageRef,
+            req.file.buffer,
+            metadata,
+        );
 
-    // Grab the public url
-    const downloadUrl = await getDownloadURL(snapshot.ref);
+        // Grab the public url
+        const downloadUrl = await getDownloadURL(snapshot.ref);
 
-    return res.send({
-      message: "file uploaded to firebase storage",
-      name: req.file.originalname,
-      downloadURL: downloadUrl,
-    });
-  } catch (error) {
-    return res.status(400).send(error.message);
-  }
+        // Extract the saved image name from the full path
+        const savedFileName = snapshot.ref.name;
+
+        return res.send({
+            message: 'file uploaded to firebase storage',
+            name: savedFileName,
+            downloadURL: downloadUrl,
+        });
+    } catch (error) {
+        return res.status(400).send(error.message);
+    }
 };
 
 exports.addImage = async (req, res) => {
-  try {
-    const dateTime = giveCurrentDateTime();
+    try {
+        const dateTime = giveCurrentDateTime();
 
-    const storageRef = ref(
-      storage,
-      `images/${req.file.originalname + "       " + dateTime}`
-    );
+        const storageRef = ref(
+            storage,
+            `images/${req.file.originalname + '       ' + dateTime}`,
+        );
 
-    const metadata = {
-      contentType: req.file.mimetype,
-    };
+        const metadata = {
+            contentType: req.file.mimetype,
+        };
 
-    // Upload the file in the bucket storage using uploadBytes
-    const snapshot = await uploadBytes(storageRef, req.file.buffer, metadata);
+        // Upload the file in the bucket storage using uploadBytes
+        const snapshot = await uploadBytes(
+            storageRef,
+            req.file.buffer,
+            metadata,
+        );
 
-    // Grab the public url
-    const downloadUrl = await getDownloadURL(snapshot.ref);
+        // Grab the public url
+        const downloadUrl = await getDownloadURL(snapshot.ref);
 
-    return res.send({
-      message: "image uploaded to firebase storage",
-      name: req.file.originalname,
-      type: req.file.mimetype,
-      downloadURL: downloadUrl,
-    });
-  } catch (error) {
-    return res.status(400).send(error.message);
-  }
+        // Extract the saved image name from the full path
+        const savedImageName = snapshot.ref.name;
+
+        return res.send({
+            message: 'image uploaded to firebase storage',
+            name: savedImageName,
+            type: req.file.mimetype,
+            downloadURL: downloadUrl,
+        });
+    } catch (error) {
+        return res.status(400).send(error.message);
+    }
 };
