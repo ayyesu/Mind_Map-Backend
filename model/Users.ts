3ad3fc5d4768957ce1e999cfd5c6cbf64193a616
ayyesu <iamdanielkissiedu@gmail.com>
2024-02-15 14:06:42 +0000

    code refactoring

--- model/Users.ts
+++ model/Users.ts
@@ -1,4 +1,4 @@
-import mongoose, {Schema, Document} from 'mongoose';
+import mongoose, {Schema} from 'mongoose';
 import {IUser} from '../interface/model/Users';
 
 const userSchema: Schema<IUser> = new Schema<IUser>({
