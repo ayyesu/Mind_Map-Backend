3c7b9157929887c45a3dd7b8871fbffb1d14c660
ayyesu <iamdanielkissiedu@gmail.com>
2024-02-15 14:26:32 +0000

    code refactoring

--- config/firebase.config.ts
+++ config/firebase.config.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Define Firebase configuration interface
+// Firebase configuration interface
 interface FirebaseConfig {
     apiKey: string;
     authDomain: string;
